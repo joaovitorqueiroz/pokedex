fd788b1359605eab06db4a626a837f8a3f895943
João Vitor Queiroz <joaovitor.queiroz@outlook.com.br>
2023-01-02 11:41:08 -0300

    test: ensure Chip component render correctly

---
+++ src/components/Chip/__tests__/Chip.test.tsx
@@ -0,0 +1,49 @@
+import Chip from '../';
+import { render } from '@testing-library/react-native';
+import { ThemeProvider } from 'styled-components/native';
+import themeDefault from 'src/styles/themes/default';
+
+jest.mock('react-native-responsive-fontsize', () => ({
+  RFValue: jest.fn(value => value),
+}));
+
+describe('Chip', () => {
+
+
+  const setup = (propOverrides: any) => {
+    const defaultProps = {
+      ...propOverrides,
+    };
+
+    const onPress = jest.fn();
+
+    return render(
+      <ThemeProvider theme={themeDefault}>
+        <Chip         
+          {...defaultProps}
+        />
+      </ThemeProvider>,
+    );
+  };
+  it('should render correctly', () => {
+    const { getByText, getByTestId,getByLabelText } =
+      setup({text: "some text", color: '#0F0', textColor: '#000'});
+
+    expect(getByText('Some text')).toBeTruthy();
+    expect(getByTestId('chip-text').props).toMatchObject({
+      style: expect.arrayContaining([
+        expect.objectContaining({ color: '#000' }),
+      ]),
+    });
+    expect(getByLabelText('chip').props).toMatchObject({
+      style: expect.arrayContaining([
+        expect.objectContaining({ backgroundColor: "#0F0" }),
+      ]),
+    });
+    expect(getByTestId('chip-text').props).toMatchObject({
+      style: expect.arrayContaining([
+        expect.objectContaining({ color: '#000' }),
+      ]),
+    });
+  });  
+});
