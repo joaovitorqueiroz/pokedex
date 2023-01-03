52a7e726be5279360085c2a7ec999d37ea20fcc6
João Vitor Queiroz <joaovitor.queiroz@outlook.com.br>
2023-01-03 19:06:36 -0300

    feat: add snackbar on requests errors

--- src/hooks/useFetchAllPokemons.ts
+++ src/hooks/useFetchAllPokemons.ts
@@ -1,10 +1,22 @@
 import { useQuery } from 'react-query';
 import { fetchAllPokemon } from 'src/services/api/pokemon';
+import Snackbar from 'react-native-snackbar';
+import { useTheme } from 'styled-components/native';
 
 export const queryKeyFetchAllPokemon = 'fetchAllPokemon';
 
 const useFetchAllPokemons = () => {
-  return useQuery(queryKeyFetchAllPokemon, fetchAllPokemon);
+  const theme = useTheme();
+
+  return useQuery(queryKeyFetchAllPokemon, fetchAllPokemon, {
+    onError: () => {
+      Snackbar.show({
+        text: 'Request failed!',
+        duration: Snackbar.LENGTH_LONG,
+        backgroundColor: theme.colors.danger,
+      });
+    },
+  });
 };
 
 export default useFetchAllPokemons;
--- src/hooks/useFetchPokemonDetailsById.ts
+++ src/hooks/useFetchPokemonDetailsById.ts
@@ -1,14 +1,28 @@
 import { useQuery } from 'react-query';
 import { fetchPokemonDetailsById } from 'src/services/api/pokemon';
+import Snackbar from 'react-native-snackbar';
+import { useTheme } from 'styled-components/native';
+import { useNavigation } from '@react-navigation/native';
 
 export const queryKeyFetchPokemonDetailsById = 'fetchPokemonDetailsById';
 
 const useFetchPokemonDetailsById = (pokemonId: number | undefined) => {
+  const theme = useTheme();
+  const navigation = useNavigation();
+
   return useQuery(
     [queryKeyFetchPokemonDetailsById, pokemonId],
     fetchPokemonDetailsById,
     {
       enabled: !!pokemonId,
+      onError: () => {
+        navigation.navigate('home');
+        Snackbar.show({
+          text: 'Request failed!',
+          duration: Snackbar.LENGTH_LONG,
+          backgroundColor: theme.colors.danger,
+        });
+      },
     },
   );
 };
