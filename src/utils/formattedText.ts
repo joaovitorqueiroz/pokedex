c2e7cadc768ca90a757007e589c9f4bfca59bc86
João Vitor Queiroz <joaovitor.queiroz@outlook.com.br>
2023-01-05 11:34:54 -0300

    refactor: formattedText

--- src/utils/formattedText.ts
+++ src/utils/formattedText.ts
@@ -1,5 +1,7 @@
 export const formattedText = (text: string | undefined) => {
-  const _text = typeof text === 'string' ? text : '';
-  // Line break remover
-  return _text.replace(/(\r\n|\r)/gm, '').replace(/(\n)/gm, ' ');
+  if (!text || typeof text !== 'string') {
+    return '';
+  }
+  // remove all white-spaces characters ([ \f\n\r\t\v​\u00a0\u1680​\u2000​-\u200a\u2028\u2029\u202f\u205f\u3000\ufeff])
+  return text.replace(/\s+/g, ' ').trim();
 };
